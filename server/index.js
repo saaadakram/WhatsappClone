b4213e21e29edf5b3138f819f5bf26df6f456cb5
saadakram <saadakram288@gmail.com>
2024-07-20 05:49:53 +0500

    Authentication

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const helmet = require("helmet")
 const server = require("http").createServer(app)
 const cors = require("cors")
 const authRouter = require("./router/authRouter")
+const session = require("express-session")
+require("dotenv").config()
 
 
 const io = new Server(server, {
@@ -21,6 +23,20 @@ app.use(cors({
         credentials: true
 }))
 app.use(express.json())
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    credentials: true,
+    name: "sid",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.environment === "production" ? "true" : "auto",
+        httpOnly: true,
+        sameSite: process.env.environment === "production" ? "none" : "lax"
+
+    }
+
+}))
 app.use("/auth", authRouter)
 app.get("/", (req,res)=>{
     res.json("I am running on port 4000")
--- server/router/authRouter.js
+++ server/router/authRouter.js
@@ -1,16 +1,68 @@
 const express = require("express");
 const validateForm = require("../controller/validateForm");
 const router = express.Router();
+const pool = require("../db");
+const bcrypt = require("bcrypt");
 
+router
+  .route("/login")
+  .get(async (req, res) => {
+    if (req.session.user && req.session.user.username) {
+      res.json({ loggedIn: true, username: req.session.user.username });
+    } else {
+      res.json({ loggedIn: false });
+    }
+  })
+  .post(async (req, res) => {
+    validateForm(req, res);
 
+    const potentialLogin = await pool.query(
+      "SELECT id, username, passhash FROM users u WHERE u.username=$1",
+      [req.body.username]
+    );
 
-
-router.post("/login", (req, res) => {
-validateForm(req,res)
-});
-
-router.post("/signup", (req, res) => {
-   validateForm(req,res)
+    if (potentialLogin.rowCount > 0) {
+      const isSamePass = await bcrypt.compare(
+        req.body.password,
+        potentialLogin.rows[0].passhash
+      );
+      if (isSamePass) {
+        req.session.user = {
+          username: req.body.username,
+          id: potentialLogin.rows[0].id,
+        };
+        res.json({ loggedIn: true, username: req.body.username });
+      } else {
+        res.json({ loggedIn: false, status: "Wrong username or password!" });
+        console.log("not good");
+      }
+    } else {
+      console.log("not good");
+      res.json({ loggedIn: false, status: "Wrong username or password!" });
+    }
   });
+
+router.post("/signup", async (req, res) => {
+  validateForm(req, res);
+  const existingUser = await pool.query(
+    "SELECT username from users where username = $1 ",
+    [req.body.username]
+  );
+  if (existingUser.rowCount === 0) {
+    //register
+    const hashPass = await bcrypt.hash(req.body.password);
+    const newUserQuery = await pool.query(
+      "INSERT INTO users(username, passhash) values ($1,$2) RETURNING id username",
+      [req.body.username, hashPass]
+    );
+    req.session.user = {
+      username,
+      id: newUserQuery.row[0].id,
+    };
+    res.json({ loggedin: true, username });
+  } else {
+    res.json({ loggedin: false, status: "username taken" });
+  }
+});
 
 module.exports = router;
